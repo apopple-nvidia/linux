fdd6519e5c8ebd5c1a27d30137c36130504cccee
Alistair Popple <apopple@nvidia.com>
2025-06-06 16:31:52 +1000

    Revert "NumExt as const"
    This reverts commit 37f4640d0ef4370c113b22479060e6db793702f0.

--- rust/kernel/num.rs
+++ rust/kernel/num.rs
@@ -3,7 +3,6 @@
 //! Numerical and binary utilities for primitive types.
 
 /// Extension trait providing useful methods for the kernel on integers.
-#[const_trait]
 pub trait NumExt {
     /// Align `self` down to `alignment`.
     ///
@@ -60,7 +59,7 @@ pub trait NumExt {
 macro_rules! numext_impl {
     ($($t:ty),+) => {
         $(
-            impl const NumExt for $t {
+            impl NumExt for $t {
                 #[inline]
                 fn align_down(self, alignment: Self) -> Self {
                     self & !alignment.wrapping_sub(1)
