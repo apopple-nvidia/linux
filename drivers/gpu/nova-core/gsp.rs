3ab80194dd07040942fb18fa1a86ff03abdda77a
Alistair Popple <apopple@nvidia.com>
2025-06-26 12:55:36 +1000

    gpu: nova-core: gsp: Add GSP command queue handling
    This commit introduces core infrastructure for handling GSP command and
    message queues in the nova-core driver. The command queue system enables
    bidirectional communication between the host driver and GSP firmware
    through a remote message passing interface.
    
    The interface is based on passing serialised data structures over a ring
    buffer with separate transmit and receive queues. Commands are sent by
    writing to the CPU transmit queue and waiting for completion via the
    receive queue.
    
    To ensure safety mutable or immutable (depending on whether it is a send
    or receive operation) references are taken on the command queue when
    allocating the message to write/read to. This ensures message memory
    remains valid and the command queue can't be mutated whilst an operation
    is in progress.
    
    Currently this is only used by the probe() routine and therefore can
    only used by a single thread of execution. Locking to enable safe access
    from multiple threads will be introduced in a future series when that
    becomes necessary.
    
    Signed-off-by: Alistair Popple <apopple@nvidia.com>
    
    ---
    
    Changes for v4:
     - Use read_poll_timeout() instead of wait_on()
     - Switch to using `init!` (Thanks Alex for figuring out the
       required workarounds)
     - Pass the enum type into the RPC bindings instead of a raw u32
     - Fixup the TODOs for extracting/allocating the send command regions
     - Split the sending functions into one taking just a command struct and
       another taking a command struct with extra payload
    
    Changes for v3:
     - Reduce the receive payloads to the correct size
     - Use opaque bindings
     - Clean up of the command queue PTE creation
     - Add an enum for the GSP functions
     - Rename GspCommandToGsp and GspMessageFromGsp
     - Rename GspCmdq
     - Add function to notify GSP of updated queue pointers
     - Inline driver area access functions
     - Fixup receive area calculations
    
    Changes for v2:
     - Rebased on Alex's latest series

--- drivers/gpu/nova-core/gsp.rs
+++ drivers/gpu/nova-core/gsp.rs
@@ -2,6 +2,7 @@
 
 mod boot;
 
+use kernel::alloc::flags::GFP_KERNEL;
 use kernel::device;
 use kernel::dma::CoherentAllocation;
 use kernel::dma::DmaAddress;
@@ -11,12 +12,15 @@ use kernel::prelude::*;
 use kernel::transmute::AsBytes;
 
 use crate::fb::FbLayout;
+use crate::gsp::cmdq::Cmdq;
 
 pub(crate) use fw::{GspFwWprMeta, LibosParams};
 
 mod fw;
 
 use fw::LibosMemoryRegionInitArgument;
+
+pub(crate) mod cmdq;
 
 pub(crate) const GSP_PAGE_SHIFT: usize = 12;
 pub(crate) const GSP_PAGE_SIZE: usize = 1 << GSP_PAGE_SHIFT;
@@ -31,6 +35,7 @@ pub(crate) struct Gsp {
     loginit: LogBuffer,
     logintr: LogBuffer,
     logrm: LogBuffer,
+    pub(crate) cmdq: Cmdq,
 }
 
 #[repr(C)]
@@ -110,11 +115,15 @@ impl Gsp {
         let logrm = LogBuffer::new(dev)?;
         dma_write!(libos[2] = LibosMemoryRegionInitArgument::new("LOGRM", &logrm.0)?)?;
 
+        // Creates its own PTE array.
+        let cmdq = Cmdq::new(dev)?;
+
         Ok(try_pin_init!(Self {
             libos,
             loginit,
             logintr,
             logrm,
+            cmdq,
         }))
     }
 }
