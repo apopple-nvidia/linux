fc181e40c07350fc876cec516b27b138d10fe674
John Hubbard <jhubbard@nvidia.com>
2025-06-29 16:27:09 -0700

    nova-core: Blackwell chipset identification

--- drivers/gpu/nova-core/gpu.rs
+++ drivers/gpu/nova-core/gpu.rs
@@ -79,6 +79,12 @@ define_chipset!({
     AD104 = 0x194,
     AD106 = 0x196,
     AD107 = 0x197,
+    // Blackwell
+    GB202 = 0x1b2,
+    GB203 = 0x1b3,
+    GB205 = 0x1b5,
+    GB206 = 0x1b6,
+    GB207 = 0x1b7,
 });
 
 impl Chipset {
@@ -92,6 +98,9 @@ impl Chipset {
             }
             Self::AD102 | Self::AD103 | Self::AD104 | Self::AD106 | Self::AD107 => {
                 Architecture::Ada
+            }
+            Self::GB202 | Self::GB203 | Self::GB205 | Self::GB206 | Self::GB207 => {
+                Architecture::Blackwell
             }
         }
     }
@@ -117,6 +126,7 @@ pub(crate) enum Architecture {
     Turing = 0x16,
     Ampere = 0x17,
     Ada = 0x19,
+    Blackwell = 0x1b,
 }
 
 impl TryFrom<u8> for Architecture {
@@ -127,6 +137,7 @@ impl TryFrom<u8> for Architecture {
             0x16 => Ok(Self::Turing),
             0x17 => Ok(Self::Ampere),
             0x19 => Ok(Self::Ada),
+            0x1b => Ok(Self::Blackwell),
             _ => Err(ENODEV),
         }
     }
