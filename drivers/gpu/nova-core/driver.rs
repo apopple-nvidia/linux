25d59d613ba5aaa6ac53a051962847b7b1e39eb9
Alistair Popple <apopple@nvidia.com>
2025-08-13 18:15:09 +1000

    nova-core: Set correct DMA mask
    Set the correct DMA mask. Without this DMA will fail on some setups.
    
    Signed-off-by: Alistair Popple <apopple@nvidia.com>

--- drivers/gpu/nova-core/driver.rs
+++ drivers/gpu/nova-core/driver.rs
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: GPL-2.0
 
-use kernel::{auxiliary, bindings, c_str, device::Core, pci, prelude::*, sizes::SZ_16M, sync::Arc};
+use kernel::{
+    auxiliary, bindings, c_str, device::Core, dma::Device, dma::DmaMask, pci, prelude::*,
+    sizes::SZ_16M, sync::Arc,
+};
 
 use crate::gpu::Gpu;
 
@@ -35,6 +38,9 @@ impl pci::Driver for NovaCore {
 
         pdev.enable_device_mem()?;
         pdev.set_master();
+
+        // SAFETY: No DMA allocations have been made yet
+        unsafe { pdev.dma_set_mask_and_coherent(DmaMask::new::<48>())? };
 
         let bar = Arc::pin_init(
             pdev.iomap_region_sized::<BAR0_SIZE>(0, c_str!("nova-core/bar0")),
